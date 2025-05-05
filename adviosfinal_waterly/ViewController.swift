0034936ab70bdeddb124682253ff662bf8e4c371
diasjakupov <dias.jakupov@zimran.io>
2025-05-06 00:16:55 +0500

    Initial Commit

---
+++ adviosfinal_waterly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  adviosfinal_waterly
+//
+//  Created by Dias Jakupov on 06.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
